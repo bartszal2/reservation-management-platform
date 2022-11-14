2d43e915a73c08f59a1d093ab15ef98da73e0b9b
bartszal2 <104868326+bartszal2@users.noreply.github.com>
2022-11-14 12:37:30 +0100

    Updates

---
+++ precache-manifest.9b71f43a3b15249d2144151fc939b0fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f0db5efd2861a6f70ef",
+    "url": "/reservation-management-platform/static/css/main.190d4994.chunk.css"
+  },
+  {
+    "revision": "2f0db5efd2861a6f70ef",
+    "url": "/reservation-management-platform/static/js/main.2f0db5ef.chunk.js"
+  },
+  {
+    "revision": "e67e65479d4f06baac99",
+    "url": "/reservation-management-platform/static/js/1.e67e6547.chunk.js"
+  },
+  {
+    "revision": "11f932a193fb1c1d61eb",
+    "url": "/reservation-management-platform/static/js/2.11f932a1.chunk.js"
+  },
+  {
+    "revision": "02891582fda8d39820d5",
+    "url": "/reservation-management-platform/static/js/runtime~main.02891582.js"
+  },
+  {
+    "revision": "23c4037f0fe2ed1ddbfdfca7c95f3025",
+    "url": "/reservation-management-platform/index.html"
+  }
+];
